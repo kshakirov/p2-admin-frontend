f694cd67d7b642ca08e2578830069d7b287cb0ca
kshakirov <kshakirov@zoral.com.ua>
2017-08-21 16:53:09 +0300

    live reload done

--- app/js/app.js
+++ app/js/app.js
@@ -1,22 +1,5 @@
 'use strict';
 
-
-// Declare app level module which depends on filters, and services
-// angular.module('myApp', ['myApp.filters', 'myApp.services', 'myApp.directives', 'ngRoute']).config(['$routeProvider', '$locationProvider', function ($routeProvider, $locationProvider) {
-//     $routeProvider.when('/view1', {templateUrl: 'partial/1', controller: MyCtrl1});
-//     $routeProvider.when('/view2', {templateUrl: 'partial/2', controller: MyCtrl2});
-//     $routeProvider.otherwise({redirectTo: '/view1'});
-//     $locationProvider.html5Mode(true);
-// }]);
-//
-// var myApp = angular.module('myApp', []);
-//
-// myApp.controller('AppCtrl', ['$scope', function ($scope) {
-//     $scope.greeting = 'Hola!';
-//     console.log("Hello");
-// }]);
-
-
 angular.module('myApp', ['ngRoute'])
 
     .controller('AppCtrl', function($scope, $route, $routeParams, $location) {
@@ -29,7 +12,7 @@ angular.module('myApp', ['ngRoute'])
 
     .controller('MyCtrl2', function($scope, $routeParams) {
         $scope.hello = "test";
-        console.log("AppCtrl2")
+        console.log("AppCtrl fdf  2")
     })
 
     .config(function($routeProvider, $locationProvider) {
