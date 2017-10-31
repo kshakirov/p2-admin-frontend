ec47819637784cdd6540f5e1fa9e773e5837735c
kshakirov <kshakirov@zoral.com.ua>
2017-10-31 20:10:41 +0200

    #153 WIP  token expiration as a parameter

--- pims_app/proxy/tokenTools.js
+++ pims_app/proxy/tokenTools.js
@@ -1,11 +1,12 @@
 let jwt = require('jsonwebtoken'),
     config = require('config'),
     pimsConfig = config.get('config'),
-    ldap_secret = pimsConfig.ldap.secret;
+    token_expiration = pimsConfig.ldap.tokenExpiration,
+    token_secret = pimsConfig.ldap.secret;
 
 function verify_token(token) {
     try {
-        var decoded = jwt.verify(token, ldap_secret);
+        var decoded = jwt.verify(token, token_secret);
         return decoded.user;
     } catch (err) {
         return false;
@@ -20,7 +21,7 @@ function get_token(headers) {
 
 
 function generate_token(name) {
-    let token = jwt.sign({user: name}, ldap_secret, {expiresIn: '1h'});
+    let token = jwt.sign({user: name}, token_secret, {expiresIn: token_expiration});
     return token;
 }
 
