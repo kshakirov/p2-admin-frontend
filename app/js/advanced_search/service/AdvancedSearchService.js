6063915617075d3a99597406a7c53fe9f2424361
kshakirov <kshakirov@zoral.com.ua>
2017-12-24 05:28:09 +0200

    WIP #262 old bug fixed

--- app/js/advanced_search/service/AdvancedSearchService.js
+++ app/js/advanced_search/service/AdvancedSearchService.js
@@ -18,7 +18,7 @@ pimsServices.service('AdvancedSearchService', ['$http', function ($http) {
 
     this.getFields = function (attributes) {
         var keys = Object.keys(attributes);
-        var fields =  keys.map(function (key) {
+        var fields = keys.map(function (key) {
             return attributes[key].uuid
         });
         return fields;
@@ -28,35 +28,37 @@ pimsServices.service('AdvancedSearchService', ['$http', function ($http) {
             r_c_ns = {},
             keys = Object.keys(referencesName);
         keys.map(function (key) {
-            if(referencesName[key].length > 0 && referencesName[key][0].attributes.length > 0) {
+            if (referencesName[key].length > 0 && referencesName[key][0].attributes.length > 0) {
                 r_ns[key] = referencesName[key][0].attributes[0].uuid.toString();
-            }else {
+            } else {
                 return false;
             }
         });
-        Object.keys(r_ns).map(function(key, index) {
-            if(r_ns[key]){
-               r_c_ns[key] = r_ns[key]
+        Object.keys(r_ns).map(function (key, index) {
+            if (r_ns[key]) {
+                r_c_ns[key] = r_ns[key]
             }
         });
         return r_c_ns;
     }
-    
+
     this.prepSearchParams = function (s_p, layout) {
         var search_params = {};
         angular.copy(s_p, search_params);
         var keys = Object.keys(search_params);
         keys.map(function (key) {
             var key_type = layout.find(function (l) {
-                if(l.uuid==key){
+                if (l.uuid == key) {
                     return l
                 }
             });
             key_type = key_type.valueType.toLowerCase();
-            if(key_type=='integer' || key_type=='decimal'){
+            if (key_type == 'integer' || key_type == 'decimal') {
 
-            }else{
-                search_params[key] = "*" + search_params[key] + "*"
+            } else {
+                if (search_params[key] && search_params[key].length > 0) {
+                    search_params[key] = "*" + search_params[key] + "*"
+                }
             }
         });
         return search_params;
