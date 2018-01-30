98d4bd2b05d2f5632e5bff6423debad3fabf7619
kshakirov <kshakirov@zoral.com.ua>
2018-01-30 17:27:44 +0200

    WIP #311 added entity type names

--- app/js/transformation_schema/TransformationSchemaListController.js
+++ app/js/transformation_schema/TransformationSchemaListController.js
@@ -1,19 +1,37 @@
 pimsApp.controller('TransformationSchemaListController', ['$scope', '$route', '$routeParams',
-    '$location', '$http', '$rootScope', 'TransformationSchemaModel','NgTableParams',
+    '$location', '$http', '$rootScope', 'TransformationSchemaModel', 'NgTableParams',
+    'EntityTypeModel', '$q',
     function ($scope, $route, $routeParams,
               $location,
               $http,
               $rootScope,
               TransformationSchemaModel,
-              NgTableParams) {
+              NgTableParams,
+              EntityTypeModel,
+              $q) {
+
 
         $scope.init = function () {
-            TransformationSchemaModel.findAll().then(function (schemata) {
+            $q.all([TransformationSchemaModel.findAll(), EntityTypeModel.findAll()]).then(function (promises) {
+                var schemata = add_enttiy_type(promises[0], promises[1]);
                 $scope.schemataTableParams = new NgTableParams({}, {dataset: schemata});
-            });
+            })
         };
 
         $scope.createSchema = function () {
             $location.path("/transformation-schemata/new");
+        };
+
+
+        function add_enttiy_type(schemata, entity_types) {
+            var schemata = schemata;
+            schemata.map(function (s) {
+                var entity_type = entity_types.find(function (et) {
+                    if (et.uuid === s.customAttributes.entity.uuid)
+                        return true
+                });
+                s.customAttributes.entity.name = entity_type.name
+            });
+            return schemata;
         }
-    }])
+    }]);
