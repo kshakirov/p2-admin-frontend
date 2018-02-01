d7b70cb85ea3b558c15f507796a43aaa6bb6da78
kshakirov <kshakirov@zoral.com.ua>
2018-02-01 20:49:10 +0200

    #319 DONE

--- app/js/external_operation/ExternalOperationListController.js
+++ app/js/external_operation/ExternalOperationListController.js
@@ -1,17 +1,42 @@
 pimsApp.controller('ExternalOperationListController', ['$scope', '$route', '$routeParams',
-    '$location', '$http', '$rootScope', 'ExternalOperationModel', 'NgTableParams', function ($scope, $route, $routeParams,
-                                                                                             $location,
-                                                                                             $http,
-                                                                                             $rootScope,
-                                                                                             ExternalOperationModel,
-                                                                                             NgTableParams) {
+    '$location', '$http', '$rootScope', 'ExternalOperationModel',
+    'NgTableParams', 'ExternalSystemModel',
+    function ($scope, $route,
+              $routeParams,
+              $location,
+              $http,
+              $rootScope,
+              ExternalOperationModel,
+              NgTableParams,
+              ExternalSystemModel) {
 
         $scope.valueTypes = ["STRING", "ARRAY", "DECIMAL"];
 
+
+        function add_external_systems(externalOperations, externalSystems) {
+            return externalOperations.map(function (eop) {
+                var ss = externalSystems.find(function (ess) {
+                    if (ess.id === eop.targetSystem.frontendInfo.selected.id)
+                        return true
+                });
+                var ts = externalSystems.find(function (ess) {
+                    if (ess.id === eop.sourceSystem.frontendInfo.selected.id)
+                        return true
+                });
+                eop.source = ss.name;
+                eop.target = ts.name;
+                return eop;
+            })
+        }
+
+
         $scope.init = function () {
             ExternalOperationModel.findAll().then(function (externalOperations) {
-                console.log(externalOperations);
-                $scope.externalOperationsTableParams = new NgTableParams({}, {dataset: externalOperations});
+                ExternalSystemModel.findAll().then(function (externalSystems) {
+                    var enriched_operations = add_external_systems(externalOperations, externalSystems);
+                    $scope.externalOperationsTableParams = new NgTableParams({}, {dataset: enriched_operations});
+                })
+
             })
         };
 
