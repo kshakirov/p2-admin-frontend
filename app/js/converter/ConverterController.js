b2aeaa7b666ca628028e2ef19e3cd02900fcbc69
kshakirov <kshakirov@zoral.com.ua>
2018-02-02 16:21:21 +0200

    #339 converters

--- app/js/converter/ConverterController.js
+++ app/js/converter/ConverterController.js
@@ -1,22 +1,24 @@
-pimsApp.controller('ConverterController' ,[ '$scope', '$route', '$routeParams',
-    '$location',  '$http','$rootScope', 'ConverterModel',  function ($scope, $route, $routeParams,
-                                                                     $location,
-                                                                     $http,
-                                                                     $rootScope,
-                                                                     ConverterModel) {
+pimsApp.controller('ConverterController', ['$scope', '$route', '$routeParams',
+    '$location', '$http', '$rootScope', 'ConverterModel', 'ngNotify',
+    function ($scope, $route, $routeParams,
+              $location,
+              $http,
+              $rootScope,
+              ConverterModel,
+              ngNotify) {
 
         $scope.types = ['Converter', 'Validator'];
         $scope.languages = [
-            {name: 'Java Script', mode:"javascript"},
-            {name: "Python", mode:"python"},
-            {name: 'Ruby', mode:"ruby"},
-            {name: 'Groovy', mode:"groovy"}
-            ];
+            {name: 'Java Script', mode: "javascript"},
+            {name: "Python", mode: "python"},
+            {name: 'Ruby', mode: "ruby"},
+            {name: 'Groovy', mode: "groovy"}
+        ];
         $scope.init = function () {
             var id = $routeParams.id;
-            if(id === "new"){
+            if (id === "new") {
 
-            }else {
+            } else {
                 ConverterModel.findOne(id).then(function (converter) {
                     $scope.converter = converter;
                 })
@@ -24,17 +26,20 @@ pimsApp.controller('ConverterController' ,[ '$scope', '$route', '$routeParams',
         };
 
         $scope.updateConverter = function (converter) {
-            if(converter.hasOwnProperty("id")) {
+            if (converter.hasOwnProperty("id")) {
                 ConverterModel.update(converter).then(function (response) {
+                    ngNotify.set("Converter Saved Successfully", 'success');
                 })
-            }else{
+            } else {
                 ConverterModel.save(converter).then(function (response) {
+                    ngNotify.set("Converter Created Successfully", 'success');
                 })
             }
         };
 
         $scope.deleteConverter = function (id) {
             ConverterModel.delete(id).then(function (response) {
+                ngNotify.set("Converter Deleted Successfully", 'success');
             })
         };
 
