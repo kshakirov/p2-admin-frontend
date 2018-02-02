f8c061770f574112349f5cf1b3167c5ba838531b
kshakirov <kshakirov@zoral.com.ua>
2018-02-02 16:16:17 +0200

    #337 filters

--- app/js/filter/FilterController.js
+++ app/js/filter/FilterController.js
@@ -1,18 +1,20 @@
-pimsApp.controller('FilterController' ,[ '$scope', '$route', '$routeParams',
-    '$location',  '$http','$rootScope', 'FilterModel', '$filter', function ($scope, $route, $routeParams,
-                                                                     $location,
-                                                                     $http,
-                                                                     $rootScope,
-                                                                  FilterModel,
-                                                                            $filter) {
+pimsApp.controller('FilterController', ['$scope', '$route', '$routeParams',
+    '$location', '$http', '$rootScope', 'FilterModel', '$filter', 'ngNotify',
+    function ($scope, $route, $routeParams,
+              $location,
+              $http,
+              $rootScope,
+              FilterModel,
+              $filter,
+              ngNotify) {
 
         $scope.types = ['OpenErp', 'Other'];
-        
+
         $scope.init = function () {
             var id = $routeParams.id;
-            if(id === "new"){
+            if (id === "new") {
 
-            }else {
+            } else {
                 FilterModel.findOne(id).then(function (filter) {
                     filter.conditions = $filter('json')(filter.conditions, 4);
 
@@ -25,17 +27,20 @@ pimsApp.controller('FilterController' ,[ '$scope', '$route', '$routeParams',
             var filter_to_save = {};
             angular.copy(filter, filter_to_save);
             filter_to_save.conditions = JSON.parse(filter.conditions);
-            if(filter_to_save.hasOwnProperty("id")) {
+            if (filter_to_save.hasOwnProperty("id")) {
                 FilterModel.update(filter_to_save).then(function (response) {
+                    ngNotify.set("Filter Saved Successfully", 'success');
                 })
-            }else{
+            } else {
                 FilterModel.save(filter_to_save).then(function (response) {
+                    ngNotify.set("Filter Created Successfully", 'success');
                 })
             }
         };
 
         $scope.deleteFilter = function (id) {
             FilterModel.delete(id).then(function (response) {
+                ngNotify.set("Filter Deleted Successfully", 'success');
             })
         };
 
